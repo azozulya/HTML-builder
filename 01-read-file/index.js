26b62805431ced5275741f11497b6f2a6997b24a
azozulya <bbbom@mail.ru>
2022-05-23 12:14:32 +0300

    fix: delete console.log

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,5 +3,4 @@ const fs = require('fs');
 
 const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 stream.on('data', (data) => console.log(data));
-stream.on('end', () => console.log('The end'));
 stream.on('error', (error) => console.log(error.message));
